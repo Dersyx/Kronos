e3ab6e4dbb7f0ea2d2edc5bc857cccbda8fc0e91
Dersyx <46288067+Dersyx@users.noreply.github.com>
2019-01-06 11:38:40 -0800

    Fixed Bugs
    Came across many bugs, and fixed them as I found them. Such bugs as output encoding error, function calling, and everything else have been fixed.

--- kronos.py
+++ kronos.py
@@ -1,5 +1,4 @@
-# Kronos is a modern, python based vulnerability scanner.
-# Main Github Page: https://github.com/Dersyx/Kronos.
+# -*- coding: utf-8 -*-
 
 # Imports
 import argparse
@@ -17,10 +16,13 @@ def main():
     Then, it adds a host argument, which is required.
     After, it takes that argument, and assigns it to variable 'hoster'
     If there is nothing in the hoster variable, it exits the program.
-    Else, it continues on with initiating function nmapScan.
+    Else, it continues on with initiating function nmap_scan.
+    From there, nmap_scan returns csv and output, which then csv_parser
+    uses to be called
     """
 
-    parser = argparse.ArgumentParser()  # Initiating the Argument Parser.
+    parser = argparse.ArgumentParser(
+        description="Kronos V1, a modern, python based vulnerability scanner by Dersyx. https://github.com/Dersyx/Kronos")
     parser.add_argument('--host', action="store", dest="host", required=True)
 
     given_args = parser.parse_args()
@@ -56,34 +58,34 @@ def nmap_scan(hoster):
 
 # Printing out the basic info gathered in the scan
     for host in nm_scanner.all_hosts():  # References host to minimize errors.
-        time_of_file = time.strftime("%m-%d-%Y-%H.%M.%S")
+        time_of_file = time.strftime("%m-%d-%Y_%H.%M.%S")
         # Opens a file with the time and host of the scan.
-        output = open(
-            time_of_file + " " + nm_scanner[host].hostname() + ".txt", "w+")
+        output = open("{}_{}.txt".format(
+            time_of_file, nm_scanner[host].hostname()), "w+", encoding="UTF-8")
         output.write('-------BASIC INFO-------\r\n')
         output.write("\r\n")
-        output.write('Host: %s (%s)\r\n' % (host, nm_scanner[host].hostname()))
-        output.write('State: %s\r\n' % nm_scanner[host].state())
+        output.write('Host: {} ({})\r\n'.format(host, nm_scanner[host].hostname()))
+        output.write('State: {}\r\n'.format(nm_scanner[host].state()))
         output.write("Found OS: " + str(nm_scanner[host].get('os')) + "\r\n")
         output.write("\r\n")
-
-    csv_parser(csv, output)  # Calls csvParser function
+        csv_parser(csv, output)  # Calls csvParser function
 
 
 def csv_parser(csv, output):
     """
     First, csvParser splits the data into a more sortable format.
-    Next, it outputs the data to a file, and then calls 'vulnersSearcher'
+    Then, it outputs to the file.
+    Finally, it calls 'vulnersSearcher'.
     """
     csv = csv.splitlines()  # Splits data
     output.write('-------SERVICES-------\r\n')  # Spacer
     output.write("\r\n")
-    port = [i.split(";")[4] for i in csv]  # Prints port number
-    name = [i.split(';')[5] for i in csv]  # Prints name of the port
-    state = [i.split(';')[6] for i in csv]  # Prints out the state of the port
-    product = [i.split(';')[7] for i in csv]  # Prints product (if available)
-    extrainfo = [i.split(';')[8] for i in csv]  # Prints extrainfo
-    version = [i.split(';')[10] for i in csv]  # Prints version
+    port = [i.split(";")[4] for i in csv]  # Sorts port number
+    name = [i.split(';')[5] for i in csv]  # Sorts name of the port
+    state = [i.split(';')[6] for i in csv]  # Sorts out the state of the port
+    product = [i.split(';')[7] for i in csv]  # Sorts product (if available)
+    extrainfo = [i.split(';')[8] for i in csv]  # Sorts extrainfo (if there)
+    version = [i.split(';')[10] for i in csv]  # Sorts version (if there)
 
     i = 0
 
@@ -91,16 +93,16 @@ def csv_parser(csv, output):
         if i == 0:  # Capitalizes the titles of the outputs
             output.write("Port Name State Extra Info Version \r\n")
         elif port[i] != port[i-1]:  # Outputs everything else as-is
-            output.write(
-                port[i] + " " + name[i] + " " + state[i] + " " +
-                product[i] + " " + extrainfo[i] + " " + version[i] + "\r\n")
+            output.write("{} {} {} {} {} {}\r\n".format(
+                port[i], name[i], state[i], product[i],
+                extrainfo[i], version[i]))
 
         i = i + 1
 
-    vulners_searcher(product, extrainfo, version, output)  # Calls next function
+    vulners_search(product, extrainfo, version, output)  # Calls next function
 
 
-def vulners_searcher(product, extrainfo, version, output):
+def vulners_search(product, extrainfo, version, output):
     """
     First, the function gets rid of dead space, and searches through the
     Vulners database.
@@ -113,41 +115,50 @@ def vulners_searcher(product, extrainfo, version, output):
         if not product[i]:  # Gets rid of blank space searches
             # Searches for exploits in the vulners database
             search = VULNERSAPI.searchExploit(
-                product[i] + " " + extrainfo[i] + " " + version[i] +
-                " order:cvss.score")
+                "{} {} {} order:cvss.score".format(
+                    product[i], extrainfo[i], version[i]))
 
             if not search:  # If a search is blank, it doesn't output to file.
                 return
-            else:
+
+            try:
                 output.write("\r\n")
                 output.write("-------VULNERABILITIES-------\r\n")  # Spacer
                 output.write("\r\n")
                 output.write("---" + product[i] + "---\r\n")
                 search = str(search).split('"')  # Splits the data into blocks.
                 search = str(search).split("', '")  # Splits the data further.
-                title = [t for t in search if "title" in t]  # Searches title.
-                href = [h for h in search if "href" in h]  # Searches for href.
-
-                for i in range(len(title)):  # Prints data length of title.
-                    try:
-                        if len(title[i]) < 100:  # Limits data.
-                            output.write(
-                                "Vuln: " + title[i].replace("'", "")
-                                .replace("title", "").replace(": ", "") +
-                                "\r\n")
-                                # Writes the title information to a file.
-                            output.write(
-                                "Link: " + href[i].replace("'", "")
-                                .replace("href", "").replace(": ", "") +
-                                "\r\n")
-                                # Writes the href information to a file.
-                            output.write("~~~~~~~\r\n")
-
-# Sometimes occurs when a title doesn't include an href, so this skips pass it.
-                    except Exception:
-                        pass
+                search_output(search, output)
+            except ValueError:
+                pass
 
         i = i + 1
+
+def search_output(search, output):
+    """
+
+    """
+    title = [t for t in search if "title" in t]  # Searches title.
+    href = [h for h in search if "href" in h]  # Searches for href.
+
+    for i in range(len(title)):  # Prints data length of title.
+        if len(title[i]) < 100:  # Limits data.
+            try:
+                output.write(
+                    "Vulnerability: {}".format(
+                        title[i].replace("'", "")
+                        .replace("title", "").replace(": ", "")))
+                output.write("\r\n")
+                # Writes the title information to a file.
+                output.write(
+                    "Link: {}".format(
+                        href[i].replace("'", "")
+                        .replace("href", "").replace(": ", "")))
+                output.write("\r\n")
+                # Writes the href information to a file.
+                output.write("~~~~~~~\r\n")
+            except IndexError:
+                pass
 
     output.close()
 
